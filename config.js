21a647be6c25b3647d4babd643da1d252cbd9ae5
fejkens <lol85251@gmail.com>
2019-01-27 19:15:29 +0000

    First edits to the template

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Wojciech Kedzior', // Required
+  description: 'Self-taught Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    github: 'https://github.com/fejkens',
+    twitter: 'https://twitter.com/fejkens',
+    linkedin: 'https://www.linkedin.com/in/wojciech-kedzior-4a3259a5/',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
 };
